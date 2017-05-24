bcd673333f475ab8de18df92e8f57cd64f12fc0f
dhackelman <drew.hackelman@gmail.com>
2017-05-23 22:59:13 -0400

    adjust links in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,13 +11,13 @@ class Header extends React.Component {
               <Link to="/"><h1 className="band__header">New Orleans Wildlife Band</h1></Link>
               <div className="band__navigation">
                   <nav>
-                    <Link to="/about"><li className="band__navigation__link">About</li></Link>
-                    <Link to="/music"><li className="band__navigation__link">Music</li></Link>
-                    <Link to="/videos"><li className="band__navigation__link">Videos</li></Link>
-                    <Link to="/photos"><li className="band__navigation__link">Photos</li></Link>
-                    <Link to="/calendar"><li className="band__navigation__link">Calendar</li></Link>
-                    <Link to="/contact"><li className="band__navigation__link">Contact</li></Link>
-                    <Link to="/links"><li className="band__navigation__link">Links</li></Link>
+                    <Link to="/neworleanswildlifeband/about"><li className="band__navigation__link">About</li></Link>
+                    <Link to="/neworleanswildlifeband/music"><li className="band__navigation__link">Music</li></Link>
+                    <Link to="/neworleanswildlifeband/videos"><li className="band__navigation__link">Videos</li></Link>
+                    <Link to="/neworleanswildlifeband/photos"><li className="band__navigation__link">Photos</li></Link>
+                    <Link to="/neworleanswildlifeband/calendar"><li className="band__navigation__link">Calendar</li></Link>
+                    <Link to="/neworleanswildlifeband/contact"><li className="band__navigation__link">Contact</li></Link>
+                    <Link to="/neworleanswildlifeband/links"><li className="band__navigation__link">Links</li></Link>
                   </nav>
                 </div>
             </div>

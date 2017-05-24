6c9c2496cc9b34ba187897bfe5d4915e57fbe227
dhackelman <drew.hackelman@gmail.com>
2017-05-23 22:56:18 -0400

    adjust ALL the routes

--- src/index.js
+++ src/index.js
@@ -17,14 +17,14 @@ ReactDOM.render((
     <Router history={browserHistory}>
         <Route path='/neworleanswildlifeband/' component={App}>
             <IndexRoute path='/' component={Landing}/>
-              <Route path='about' component={About}/>
-              <Route path='music' component={Music}/>
-              <Route path='videos' component={Videos}/>
-              <Route path='photos' component={Photos}/>
-              <Route path='calendar' component={Calendar}/>
-              <Route path='contact' component={Contact}/>
-              <Route path='links' component={Links}/>
-              <Route path="*" component={NotFound}/>
+              <Route path='/neworleanswildlifeband/about' component={About}/>
+              <Route path='/neworleanswildlifeband/music' component={Music}/>
+              <Route path='/neworleanswildlifeband/videos' component={Videos}/>
+              <Route path='/neworleanswildlifeband/photos' component={Photos}/>
+              <Route path='/neworleanswildlifeband/calendar' component={Calendar}/>
+              <Route path='/neworleanswildlifeband/contact' component={Contact}/>
+              <Route path='/neworleanswildlifeband/links' component={Links}/>
+              <Route path="/neworleanswildlifeband/*" component={NotFound}/>
         </Route>
     </Router>
 ), document.getElementById('main'));

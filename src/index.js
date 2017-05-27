89146d35228b97fafb1884be312bd6ea853194ae
dhackelman <drew.hackelman@gmail.com>
2017-05-26 21:06:00 -0400

    update index.js with working landing

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import NotFound from './components/NotFound';
 ReactDOM.render((
     <Router history={browserHistory}>
         <Route path='/neworleanswildlifeband/' component={App}>
-            <IndexRoute path='/'/>
+            <IndexRoute path='/neworleanswildlifeband/'/>
               <Route path='/music' component={Music}/>
               <Route path='/landing' component={Landing}/>
               <Route path='/videos' component={Videos}/>

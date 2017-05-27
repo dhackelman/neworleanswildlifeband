bd8a294c786a00f113d7c64f6049cdd45daba3e3
dhackelman <drew.hackelman@gmail.com>
2017-05-26 21:10:10 -0400

    testing route with react and GH

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ import NotFound from './components/NotFound';
 
 ReactDOM.render((
     <Router history={browserHistory}>
-        <Route path='/neworleanswildlifeband/' component={App}>
-            <IndexRoute path='/landing/'/>
+        <Route path='/' component={App}>
+            <IndexRoute path='/landing'/>
               <Route path='/music' component={Music}/>
               <Route path='/landing' component={Landing}/>
               <Route path='/videos' component={Videos}/>
